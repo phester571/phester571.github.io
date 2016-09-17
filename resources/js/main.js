5a7def6e79574b09fcff38b6b3277878cd3f72b8
Philip Murphy <gitacctmail@gmail.com>
2016-09-17 02:45:12 -0400

    Further progress

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,13 +1,14 @@
-$(function(){
-  $('a[href^="#"]').onclick(function(event){
-    var target = $(this).attr('href');
-    var strip = target.slice(1);
-    var anchor = $("a[name='" + strip + "']");
-
-    event.preventDefault();
-    
-    $('html, body').animate(function(){
-      scrollTop: anchor.offset().top
-    }, 'slow');
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
   });
 });
